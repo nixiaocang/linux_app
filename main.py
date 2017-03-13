80b4220ea69eb08e8a6f2cc873c9ab468ec90fb2
焦国富 <jiaoguofu@haizhi.com>
2017-03-13 20:06:39 +0800

    findly test

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 # -*- coding:utf-8 -*-
 
 import sys
-from fupload import FuploadHelper
+from  bdp_batch_up import FuploadHelper
+from bdp_batch_up_2 import FuploadHelper2
 from append import ApendHelper
 
 if __name__=='__main__':
     print """
-    Usage: python op confile
+    Usage: python way op confile
+    way: 0|1 文件落盘与否 0 不落盘 1 落盘
     op : create | append | retry
     confile:
         op:create
@@ -15,37 +17,44 @@ if __name__=='__main__':
             domain=xxx
             username=xxx
             password=xxx
-            ds_name=xxx
-            tbname=xxx
-            separator=x
-            null_holder=xxx
-            path=xxxx
+            path=xxxx   # 数据文件夹上层路径
         op:append
         confile include:
             domain=xxx
             username=xxx
             password=xxx
-            ds_id=xxx
-            tb_id=xxx
-            separator=x
-            null_holder=xxx
-            path=xxxx
+            ds_name=xxx
+            tbname=xxx
+            path=xxxx   # 数据文件夹路径
         op:retry
-        confile include nothing
+        confile include:
+        domain=xxx
+        username=xxx
+        password=xxx
+        path=xxx # 数据文件夹上层路径
     """
     if len(sys.argv) == 1:
         exit()
-    op = sys.argv[1]
-    conf = sys.argv[2]
+    way = sys.argv[1]
+    op = sys.argv[2]
+    conf = sys.argv[3]
+    if str(way) == '0':
+        upload_helper = FuploadHelper
+    elif str(way) == "1":
+        upload_helper = FuploadHelper2
+    else:
+        print '不支持的方式'
+        sys.exit()
+
     if op == 'create':
-        params = ['domain', 'username', 'password', 'path', 'tbname', 'ds_name', 'separator', 'null_holder']
+        params = ['domain', 'username', 'password', 'path']
     elif op == 'append':
-        params = ['domain', 'username', 'password', 'path', 'tb_id', 'ds_id', 'separator', 'null_holder']
+        params = ['domain', 'username', 'password', 'path', 'tbname', 'ds_name']
     elif op == 'retry':
-        pass
+        params = ['domain', 'username', 'password', 'path']
     else:
         print 'error op type'
-        exit()
+        sys.exit()
     fo = open('%s' % conf, 'rb').readlines()
     bag = {}
     for line in fo:
@@ -55,18 +64,14 @@ if __name__=='__main__':
             if line.find('%s=' % param) == 0:
                 bag[param] = line.split('%s=' % param)[1]
                 break
-    if op in ('create', 'append'):
-        path = bag['path']
-        separator = bag['separator']
-        null_holder = bag['null_holder']
-        ds_id = bag.get('ds_id')
-        tb_id = bag.get('tb_id')
-        ds_name = bag.get('ds_name')
-        tbname = bag.get('tbname')
+    if op == 'append':
+        tbname = bag['tbname']
+        ds_name = bag['ds_name']
+    path = bag['path']
     if op == 'create':
-        FuploadHelper(bag['domain'], bag['username'], bag['password']).do_action(path, tbname, ds_name, separator, null_holder)
+        upload_helper(bag['domain'], bag['username'], bag['password']).create(path)
     elif op == 'append':
-        ApendHelper(bag['domain'], bag['username'], bag['password']).do_action(path, separator, null_holder, ds_id, tb_id)
+        upload_helper(bag['domain'], bag['username'], bag['password']).append(path, ds_name, tbname)
     elif op == 'retry':
-        FuploadHelper(bag['domain'], bag['username'], bag['password']).retry(path)
+        upload_helper(bag['domain'], bag['username'], bag['password']).retry(path)
 
